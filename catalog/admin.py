293eb813fd2646fb07bcf3d8d45b78bceacbee56
amiroo <amir.m.raei@gmail.com>
2020-12-05 16:58:53 +0330

    'create models of project'

--- catalog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Article
-from .models import Author
-
-# register models to use in admin site
-admin.site.register(Article)
-admin.site.register(Author)
--- catalog/models.py
+++ catalog/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-    """Table schema to store articles."""
-    name = models.CharField(max_length=64)
-    author = models.ForeignKey('catalog.Author', on_delete=models.CASCADE)
-    content = models.TextField()
-    slug = models.CharField(default='', max_length=64)
-
-    def __str__(self):
-        return '%s' % self.name
-
-
-class Author(models.Model):
-    """Table schema to store auhtors."""
-    name = models.CharField(max_length=64)
-    slug = models.CharField(default='', max_length=64)
-
-    def __str__(self):
-        return '%s' % self.name
+    article_id = models.AutoField(primary_key=True)
+    article_heading = models.CharField(max_length=250)
+    article_body = models.TextField()
