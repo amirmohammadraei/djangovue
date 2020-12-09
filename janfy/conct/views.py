f072a1e950726935063234680e38393f990db4b2
amiroo <amir.m.raei@gmail.com>
2020-12-09 10:55:48 +0330

    backend

--- janfy/conct/views.py
+++ janfy/conct/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from requests.auth import HTTPBasicAuth
+import requests
 
-# Create your views here.
+def tess(request):
+    context={"token":"new"}
+
+    return
+
+
+def call_api(request):
+    count = 1
+    array = []
+    loop = 0
+    while loop == 0:
+        try:
+            url = '' + str(count)
+            r = requests.get(url, auth=HTTPBasicAuth('username', 'password'))
+            array.append(r.json())
+            r.json()['results']
+            count += 1
+        except KeyError:
+            loop = 1
+
+    print(array[3])
+    return HttpResponse(array)
